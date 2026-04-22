731424e78c4532bf056840c891411575d09226ab
superlmj5 <superlmj5@gmail.com>
2026-04-23 01:28:21 +0900

    [feat] Add floor Art
    Why made this like this method? I can't understand easily

--- Assets/Project/Runtime/Debug/TeamAreaViewer.cs
+++ Assets/Project/Runtime/Debug/TeamAreaViewer.cs
@@ -4,6 +4,11 @@ using UnityEngine.Tilemaps;
 public class TeamAreaViewer : MonoBehaviour
 {
     [SerializeField] private Tilemap tilemap;
+    [SerializeField] private TeamData teamA;
+    [SerializeField] private TeamData teamB;
+
+    [SerializeField] private TileBase teamATile;
+    [SerializeField] private TileBase teamBTile;
 
     public void ColorTiles(MapData mapData)
     {
@@ -12,6 +17,16 @@ public class TeamAreaViewer : MonoBehaviour
             foreach (var tile in region.MapTiles)
             {
                 tilemap.SetTileFlags((Vector3Int)tile.CellPos, TileFlags.None);
+
+                if (region is Storage)
+                {
+                    TileBase teamTile = region.OwnedTeam == teamA ? teamATile : teamBTile;
+                    if (teamTile != null)
+                    {
+                        tilemap.SetTile((Vector3Int)tile.CellPos, teamTile);
+                    }
+                }
+
                 tilemap.SetColor((Vector3Int)tile.CellPos, region.OwnedTeam.TeamColor);
             }
         }
